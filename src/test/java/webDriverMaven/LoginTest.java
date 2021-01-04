45aa010b5004bbbd45c2a274896336020531dfaf
Gunit Thapar <gunit07thapar@gmail.com>
2021-01-04 23:10:35 +0530

    Initial commit

---
+++ src/test/java/webDriverMaven/LoginTest.java
@@ -0,0 +1,34 @@
+package webDriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	@BeforeSuite
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		
+	}
+	
+	@Test
+	public void doLogin() {
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("gunit07thapar@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("sasdffds");
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
